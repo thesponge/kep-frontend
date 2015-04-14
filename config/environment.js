a643a662cc1b431ce2f049f4b03f6b293323a7f1
Victor Nițu <victor@thesponge.eu>
2015-04-14 20:04:44 +0300

    API namespace for development

--- config/environment.js
+++ config/environment.js
@@ -25,7 +25,7 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     ENV.APP.apiHost = 'http://localhost:3000';
-    ENV.APP.apiNamespace = 'api/v1';
+    ENV.APP.apiNamespace = 'v1';
     // ENV.APP.LOG_RESOLVER = true;
     ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
