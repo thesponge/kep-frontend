b95474611da9d4be8adfc7c49cd641e7e4ce6e9a
Victor Nițu <victor@thesponge.eu>
2015-05-20 12:40:41 +0300

    Fix deprecation warnings for ember-cli 0.2.5

--- app/controllers/apps.js
+++ app/controllers/apps.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   //model: function() {
   //  return this.store.find('user');
   //}.property(),
